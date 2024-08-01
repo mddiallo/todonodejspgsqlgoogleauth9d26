dd55ec49d8b3cdc07d0e12c2818ade4987f932ac
Mouhamadou DIALLO <mouhamadou.diallo@hotmail.com>
2024-08-01 15:54:50 +0200

    Update app.js
    uncomment

--- server/app.js
+++ server/app.js
@@ -38,7 +38,7 @@ const getRoles = (req, res) => {
   }
 
   // Uncomment this code below and plug in your own alias to test the contributor role
-  /*
+  
   if (body.userDetails === 'm0110110101100101@gmail.com') {
     res.send({
       "roles": [
@@ -48,7 +48,6 @@ const getRoles = (req, res) => {
 
     return;
   }
-  */
 
   res.send({
     "roles": [
